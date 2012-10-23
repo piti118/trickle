24ee3a8cd9ca954ff1e4e84357f579c7e419be18
piti <piti118@gmail.com>
2012-10-22 21:15:16 -0700

    add a bunch of options

--- trickle.py
+++ trickle.py
@@ -1,25 +1,30 @@
 from subprocess import check_output,call,STDOUT
 from argparse import ArgumentParser
 from time import sleep
+import sys
 #check the que making sure there are less than
 #n jobs enqueued in the que before excute next commnad
-def trickle(jobs, n=1000, nsub=100):
+def trickle(jobs, n=1000, nsub=100, simulate=False, que=''):
     shouldcheck = True
+    njobs = len(jobs)
     for ijob,job in enumerate(jobs):
         nque = n+1
         while shouldcheck:
-            nque = getNque()
+            nque = getNque(que)
             if nque > n:
-                print 'Too many jobs in que(%d) sleep for 1 min'%nque
+                print 'Too many jobs in que(%d) sleep for 1 min %d/%d'%(nque,ijob,njobs)
+                sys.stdout.flush()
                 sleep(60)
             else:
                 break
-        call(job,shell=True)
+        print '%d/%d: %s'%(ijob,njobs,job.strip())
+        sys.stdout.flush()
+        if not simulate: call(job.strip(),shell=True)
         shouldcheck = ijob%10==9
 
 
-def getNque():
-    cmd = 'qstat -q'
+def getNque(que):
+    cmd = 'qstat -q %s'%que
     out = check_output(cmd, shell=True)
     tmp = out.split('\n')[-2]
     return int(tmp.split()[1])
@@ -28,13 +33,16 @@ def getNque():
 def main():
     parser = ArgumentParser()
     parser.add_argument('jobfiles',nargs='+')
+    parser.add_argument('--simulate',action="store_true")
+    parser.add_argument('-n','--maxque',type=int,default=1000)
+    parser.add_argument('-q','--que',default='')
     args = parser.parse_args()
     jobs = []
     for jobfile in args.jobfiles:
         with open(jobfile) as f:
             jobs += f.readlines()
     jobs = [j for j in jobs if not j.strip().startswith('#')]
-    trickle(jobs)
+    trickle(jobs, n=args.maxque, simulate=args.simulate, que=args.que)
 
 
 if __name__ == "__main__":
